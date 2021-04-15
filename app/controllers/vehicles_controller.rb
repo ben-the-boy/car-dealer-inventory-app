da539dd521963ab96b2c2df5a3ad1e3fb28b1dc5
Ben Hayes <b.t.hayes@comcast.net>
2021-04-15 17:00:12 -0500

    "Added additional routes to vehicles controller"

--- app/controllers/vehicles_controller.rb
+++ app/controllers/vehicles_controller.rb
@@ -15,18 +15,24 @@ class VehiclesController < ApplicationController
   end
 
   get '/vehicles/:id' do
+    @vehicle = Vehicle.find(params[:id])
     erb :"vehicles/show"
   end
 
   get '/vehicles/:id/edit' do
+    @vehicle = Vehicle.find(params[:id])
     erb :"vehicles/edit"
   end
 
   patch '/vehicles/:id' do
-
+    @vehicle = Vehicle.find(params[:id])
+    @vehicle.update(params[:vehicle])
+    redirect "/vehicles/#{@vehicle.id}"
   end
 
-  delete '/vehicles/:id' do
-
+  delete '/vehicles/:id/delete' do
+    @vehicle = Vehicle.find(params[:id])
+    @vehicle.delete
+    redirect '/vehicles'
   end
 end
